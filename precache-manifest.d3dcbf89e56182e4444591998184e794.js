8a6c6eea79a9182ab414568b5115eee52a8b884a
Wave Cantery Ambray <wacanam@gmail.com>
2020-12-28 23:25:47 +0800

    Updates

---
+++ precache-manifest.d3dcbf89e56182e4444591998184e794.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cfcf505c99401a1b2347db1030a2cd3",
+    "url": "/react_demo/index.html"
+  },
+  {
+    "revision": "52b2287ee5371c4181a3",
+    "url": "/react_demo/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "52b2287ee5371c4181a3",
+    "url": "/react_demo/static/js/2.228a1520.chunk.js"
+  },
+  {
+    "revision": "34afbd88abe530019f9c9d502a55a869",
+    "url": "/react_demo/static/js/2.228a1520.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36057d369c3f00902f05",
+    "url": "/react_demo/static/js/main.5d9b0d1e.chunk.js"
+  },
+  {
+    "revision": "ee423916bac96448e430",
+    "url": "/react_demo/static/js/runtime-main.edc6025d.js"
+  }
+]);
